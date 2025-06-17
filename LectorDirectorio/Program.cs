bd0152d149919b688ec0379de67e40602117a2c1
MGArnedo <mguaarnedo@gmail.com>
2025-06-17 16:51:38 -0300

    Terminado ejercicio 1: Ahora se crea un archivo .csv que contiene informacion de los archivos dentro del directorio, informacion como su nombre, tamaño en KB y ultima modificación

--- LectorDirectorio/Program.cs
+++ LectorDirectorio/Program.cs
@@ -29,26 +29,34 @@ while (true)
 }
 /*■ Todas las carpetas que se encuentran en ese path
 Solo el nombre de carpeta*/
-Console.WriteLine("Carpetas dentro de la ruta: ");
+Console.WriteLine("Carpetas dentro del directorio: ");
 string[] carpetas = Directory.GetDirectories(path);
+int i = 1;
 foreach (string carpeta in carpetas)
 {
-    FileInfo info = new FileInfo (carpeta);
-    Console.WriteLine($"Nombre: {info.Name}");
+
+    FileInfo info = new FileInfo(carpeta);
+    Console.WriteLine($"(Carpeta {i}) Nombre: {info.Name}");
+    i++;
 }
 /*■ Todos los archivos que se encuentran directamente en esa carpeta
 Junto a cada nombre de archivo, se deberá mostrar su tamaño en
 kilobytes (KB).*/
-Console.WriteLine("Archivos dentro de la ruta: ");
+Console.WriteLine("Archivos dentro del directorio: ");
 string[] archivos = Directory.GetFiles(path);
+i = 1;
 foreach (string archivo in archivos)
 {
-    FileInfo info = new FileInfo (archivo);
-    Console.WriteLine($"Nombre: {info.Name} - Tamaño: {info.Length} Kb");
+    FileInfo info = new FileInfo(archivo);
+    Console.WriteLine($"(Archivo {i}) Nombre: {info.Name} - Tamaño: {info.Length / 1000} Kb");
+    i++;
 }
 /*○ Después de listar los archivos, el programa creará un archivo con extensión csv,
 llamado "reporte_archivos.csv" en el mismo directorio que se está analizando
 (use ruta relativa para el path del mismo).
+*/
+string rutaCsv = Path.Combine(path, "reporte_archivos.csv");
+/*
 ○ Este archivo CSV deberá contener la siguiente información en columnas
 separadas:
 ■ Nombre del Archivo: El nombre completo del archivo (incluyendo su
@@ -56,3 +64,12 @@ extensión).
 ■ Tamaño (KB): El tamaño del archivo, redondeado a dos decimales.
 ■ Fecha de Última Modificación: La fecha y hora en que el archivo fue
 modificado por última vez.*/
+using (StreamWriter writer = new StreamWriter(rutaCsv, append: true))
+{
+    writer.WriteLine("Nombre - Tamaño (KB) - Fecha de ultima modificacion");
+    foreach (string archivo in archivos)
+    {
+        FileInfo info = new FileInfo(archivo);
+        writer.WriteLine($"{info.Name} - {info.Length / 1000:F2} - {info.LastWriteTime}");
+    }
+}
