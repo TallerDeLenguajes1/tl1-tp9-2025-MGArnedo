aea6a1c019660ffbe226b50ec09ea71235f39629
MGArnedo <mguaarnedo@gmail.com>
2025-06-17 22:18:10 -0300

    Terminado ejercicio 2: Se agrego un archivo .mp3 en la carpeta, se creo un programa para poder leer el tag del mismo y ademas muestra por pantalla el nombre, artista, album y año de la cancion

---
+++ LectorTagMP3/Program.cs
@@ -0,0 +1,28 @@
+﻿using Id3v1Tag;
+using System.IO;
+using System.Text;
+Console.WriteLine("===LECTOR TAG ARCHIVO MP3===");
+string path = @"F:\taller1\tp9\tl1-tp9-2025-MGArnedo\LectorTagMP3\Silence_PresenceOfMusic.mp3";
+if (File.Exists(path))
+{
+    FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+    fs.Seek(-128, SeekOrigin.End);
+    byte[] tagBytes = new byte[128];
+    fs.Read(tagBytes, 0, tagBytes.Length);
+
+    string titulo = Encoding.GetEncoding("latin1").GetString(tagBytes, 3, 30).Trim();
+    string artista = Encoding.GetEncoding("latin1").GetString(tagBytes, 33, 30).Trim();
+    string album = Encoding.GetEncoding("latin1").GetString(tagBytes, 63, 30).Trim();
+    string anioDeLaCancion = Encoding.GetEncoding("latin1").GetString(tagBytes, 93, 4).Trim();
+
+    var tag = new Id3v1Tag.Id3v1Tag(titulo, artista, album, anioDeLaCancion);
+    Console.WriteLine($"Titulo: {tag.Titulo}");
+    Console.WriteLine($"Artista: {tag.Artista}");
+    Console.WriteLine($"Album: {tag.Album}");
+    Console.WriteLine($"Año: {tag.AnioDeLaCancion}");
+    
+} else
+{
+    Console.WriteLine("El directorio no existe.");
+}
+Console.WriteLine("===FIN DEL PROGRAMA===");
